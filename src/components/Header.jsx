a2a9bdf8b90065694dcd005419cad993f0946df3
jo0ker64 <martel.jonathan64@gmail.com>
2025-06-17 14:56:07 +0200

    React

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Header() {
     return (
         <header className="bg-gray-900 text-white p-4 shadow-md flex justify-between items-center">
-            <h1 className="text-2xl font-bold">🎓 Gestion Formation</h1>
+            <h1 className="text-2xl font-bold">Gestion Formation</h1>
             <nav className="space-x-4">
                 <Link to="/" className="hover:text-green-400">Accueil</Link>
                 <Link to="/login" className="hover:text-green-400">Connexion</Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div className="flex items-center justify-center min-h-[calc(100vh-64px)] bg-gradient-to-br from-purple-700 to-green-600 text-white">
-            <h2 className="text-4xl font-bold">Bienvenue sur le dashboard 🎉</h2>
+            <h2 className="text-4xl font-bold">Bienvenue sur le dashboard !!!</h2>
         </div>
     );
 }
